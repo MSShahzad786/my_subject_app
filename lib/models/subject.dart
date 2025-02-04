2bf4d3a851c846d7220bd7bf7e92824c90e6fec3
MSShahzad786 <shahzad4469176@gmail.com>
2025-02-04 14:33:55 +0500

    Update subject.dart

--- lib/models/subject.dart
+++ lib/models/subject.dart
@@ -1,1 +1,23 @@
+class Subject {
+  final String id;
+  final String name;
+  bool isPinned;
 
+  Subject({
+    required this.id,
+    required this.name,
+    this.isPinned = false,
+  });
+
+  factory Subject.fromJson(Map<String, dynamic> json) => Subject(
+        id: json['id'],
+        name: json['name'],
+        isPinned: json['isPinned'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'isPinned': isPinned,
+      };
+}
